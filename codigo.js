c7a710309a8d213dd37820df1848c58fd5d3d22b
Nacho Arias <ariasn93@gmail.com>
2023-12-07 23:59:42 +0100

    proyecto

---
+++ codigo.js
@@ -0,0 +1,27 @@
+// Futbol
+document.addEventListener("DOMContentLoaded", () => {
+
+   const button = document.querySelector(".button");
+button.addEventListener("click",()=>{
+   alert("Usted ha reservado en Area Chica");
+});
+
+const button2 = document.querySelector(".button2");
+button2.addEventListener("click",()=>{
+   alert("Usted ha reservado en Cardenal Spinola");
+});
+
+// Padel
+
+const button3 = document.querySelector(".button3");
+button3.addEventListener("click", () => {
+    alert("Usted ha reservado en Area Chica");
+});
+
+
+const button4 = document.querySelector(".button4");
+button4.addEventListener("click", () => {
+    alert("Usted ha reservado en Punta Verde");
+});
+
+});
