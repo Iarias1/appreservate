f2165743bdc2173a81c513f5e254539720cc5dc1
Iarias1 <143567369+Iarias1@users.noreply.github.com>
2023-12-21 22:28:35 +0100

    codigo.js

--- codigo.js
+++ codigo.js
@@ -1,27 +1,33 @@
-// Futbol
-document.addEventListener("DOMContentLoaded", () => {
-
-   const button = document.querySelector(".button");
-button.addEventListener("click",()=>{
-   alert("Usted ha reservado en Area Chica");
-});
-
-const button2 = document.querySelector(".button2");
-button2.addEventListener("click",()=>{
-   alert("Usted ha reservado en Cardenal Spinola");
-});
-
-// Padel
-
-const button3 = document.querySelector(".button3");
-button3.addEventListener("click", () => {
-    alert("Usted ha reservado en Area Chica");
-});
+function agrandarFoto (src) {
+   var imagenAmpliada = document.getElementById("imagen-ampliada");
+   imagenAmpliada.src = src;
+   document.getElementById('imagenes').style.display = 'block';
+}
+function cerrarImagen() {
+   document.getElementById('imagenes').style.display = 'none';
+ }
 
 
-const button4 = document.querySelector(".button4");
-button4.addEventListener("click", () => {
-    alert("Usted ha reservado en Punta Verde");
-});
+function reservarCancha() {
+   var select = document.getElementById("horariosf");
+   var selectedOption = select.options[select.selectedIndex].value;
+   alert("Reservó cancha en Area Chica a las " + selectedOption);
+}
 
-});
+function reservarCanchaf() {
+   var select = document.getElementById("horariosf2");
+   var selectedOption = select.options[select.selectedIndex].value;
+   alert("Reservó cancha en Cardenal Spignola a las " + selectedOption);
+}
+
+function reservarCanchaP() {
+   var select = document.getElementById("horariosp");
+   var selectedOption = select.options[select.selectedIndex].value;
+   alert("Reservó cancha en Area Chica a las " + selectedOption);
+}
+
+function reservarCanchaP2() {
+   var select = document.getElementById("horariospa");
+   var selectedOption = select.options[select.selectedIndex].value;
+   alert("Reservó cancha en Punta Verde a las " + selectedOption);
+}
